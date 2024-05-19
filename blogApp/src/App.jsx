00a0fb48a91b8da9d7353e0a604ea45bb1f9f481
Tufan <tufanpande@gmail.com>
2024-05-19 22:00:40 +0545

    APP

--- blogApp/src/App.jsx
+++ blogApp/src/App.jsx
@@ -1,16 +1,20 @@
-import Home from "./pages/home";
-import Button from "./pages/Button";
+// import Home from "./pages/home";
+// import Button from "./pages/Button";
+import Question from "./pages/Question";
+import UseEff from "./pages/UseEff";
 
 function App() {
-  const users = [
-    { name: "tufan", age: 20 },
-    { name: "dev", age: 12 },
-    { name: "rahul", age: 30 },
-  ]; //API call
+  // const users = [
+  //   { name: "tufan", age: 20 },
+  //   { name: "dev", age: 12 },
+  //   { name: "rahul", age: 30 },
+  // ]; //API call
   return (
     <>
-      <Home data={users} />
-      <Button />
+      {/* <Home data={users} />
+      <Button /> */}
+      <Question />
+      <UseEff />
     </>
   );
 }
